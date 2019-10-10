785c707abb03b4dc9357a3af87071f43b6408aef
Tiago Serique Valadares <seriquevaladares29@gmail.com>
2019-10-09 23:21:04 -0300

    Update questao19.pas

--- alg1/aulas/questao19.pas
+++ alg1/aulas/questao19.pas
@@ -12,3 +12,13 @@ begin
 	digito:= (n*10) + (c + 3*d + 5*u) mod 7;
 	writeln(digito);	
 end.
+//EXERCICIO
+//Dado um numero de tres digitos, construir outro numero de quatro 
+//digitos com a seguinte regra: a) os tres primeiros digitos, contados 
+//da esquerda para a direita, sao iguais aos do numero dado; b) o quarto 
+//digito e' um digito de controle calculado da seguinte forma: primeiro 
+//digito + 3*segundo digito + 5*terceiro digito; o digito de controle e' 
+//igual ao resto da divisao dessa soma por 7.
+//Exemplo:
+//354
+//3543
