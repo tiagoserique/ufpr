ee3ae45e904f18f5347dca945a8cfc2e5516fde2
Tiago Serique Valadares <seriquevaladares29@gmail.com>
2019-10-09 23:19:28 -0300

    Update questao18.pas

--- alg1/aulas/questao18.pas
+++ alg1/aulas/questao18.pas
@@ -12,3 +12,24 @@ begin
 	else
 		writeln('exame');
 end.
+//EXERCICIO:
+//Escreva um programa em Pascal que leia as duas notas bimestrais 
+//de um aluno e determine a sua media. A partir da media, o programa 
+//deve imprimir a seguinte mensagem: APROVADO, REPROVADO ou EXAME.  
+//A media e' 7 para Aprovacao, menor que 3 para Reprovacao e as demais em Exame.
+//Exemplos:
+//3.1
+//2.5
+//REPROVADO
+//3
+//3
+//EXAME
+//10
+//0
+//EXAME
+//6
+//8
+//APROVADO
+//10
+//10
+//APROVADO
