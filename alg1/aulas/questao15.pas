a1b3d000267e4547202341b4d4794e3fee3b83b5
Tiago Serique Valadares <seriquevaladares29@gmail.com>
2019-10-09 23:15:06 -0300

    Update questao15.pas

--- alg1/aulas/questao15.pas
+++ alg1/aulas/questao15.pas
@@ -19,3 +19,7 @@ begin
 	else
 		writeln('origem')
 end.
+//EXERCICIO: Escreva um programa para ler as coordenadas (X,Y ) de um 
+//ponto no sistema cartesiano e escrever o quadrante ao qual o ponto pertence. 
+//Caso o ponto nÃ£o pertenca a nenhum quandrante, escrever se ele estÃ¡ sobre 
+//o eixo X, eixo Y ou na origem.
