edd567e8e24629b5141e90465934ae36ae954b93
tiago <seriquevaladares29@gmail.com>
2020-10-28 17:35:47 -0300

    tira pasta com caderno de exercicios

--- caderno de exercicios/questoes01/questao01-02.pas
+++
@@ -1,8 +0,0 @@
-program questao2;
-var
-	n,soma:integer;
-begin
-	read(n);
-	soma:= n*(n+1) div 2;
-	writeln('Soma: ',soma);
-end.
--- caderno de exercicios/questoes01/questao01-04.pas
+++
@@ -1,9 +0,0 @@
-program questao4;
-var
-	x,n:integer;
-begin
-	read(x);
-	read(n);
-	if n > 0 then
-		write(x,'^',n,'=',exp(n*ln(x)):0:0);
-end.
