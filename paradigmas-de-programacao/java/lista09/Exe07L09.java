a9a4e488a389173c4554d27c087f2dfe8eb31887
tiagoserique <seriquevaladares29@gmail.com>
2022-07-05 00:49:17 -0300

    Adiciona exercicio 7 da lista 09 de paradigmas

---
+++ paradigmas-de-programacao/java/lista09/Exe07L09.java
@@ -0,0 +1,35 @@
+
+class Exe07L09 {
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+
+        lista.printList();
+        lista.insert("1", 0);
+        lista.printList();
+        System.out.println("tamanho: " + lista.size() + "\n");
+        lista.insert("2", 1);
+        lista.printList();
+        System.out.println("tamanho: " + lista.size() + "\n");
+        lista.insert("3", 2);
+        lista.printList();
+
+        System.out.println("Elemento na posicao 1: " + lista.elementAt(1));
+        System.out.println("Elemento na posicao 2: " + lista.elementAt(2));
+        System.out.println("Elemento na posicao 3: " + lista.elementAt(3));
+        System.out.println("Elemento na posicao 0: " + lista.elementAt(0));
+
+        System.out.println("tamanho: " + lista.size() + "\n");
+        lista.remove(2);
+        lista.printList();
+        System.out.println("tamanho: " + lista.size() + "\n");
+        lista.remove(1);
+        lista.printList();
+        System.out.println("tamanho: " + lista.size() + "\n");
+        lista.remove(0);
+        lista.printList();
+        System.out.println("tamanho: " + lista.size() + "\n");
+        lista.remove(0);
+        lista.printList();
+        System.out.println("tamanho: " + lista.size() + "\n");
+    }
+}
---
+++ paradigmas-de-programacao/java/lista09/Lista.java
@@ -0,0 +1,106 @@
+public class Lista {
+    String nome = "lista";
+    Nodo inicio = null;
+    int size    = 0;
+
+    public Lista(){}
+
+    public void insert(String elemento, int pos){
+        Nodo novo_nodo = new Nodo();
+        novo_nodo.elemento = elemento; 
+
+        if ( pos >= 1 ){
+            Nodo aux = this.inicio;
+            int i = 1;
+            
+            while ( ( i < pos ) && ( aux.proximo != null ) ){
+                aux = aux.proximo;
+                i++;
+            }
+            
+            novo_nodo.proximo = aux.proximo;
+            aux.proximo = novo_nodo;
+        }
+        else {
+            novo_nodo.proximo = this.inicio;
+            this.inicio = novo_nodo;            
+        }
+
+        this.size += 1;
+    }
+
+
+    public void remove(int pos){
+        if ( this.size > 0 ){
+            Nodo deletado_nodo, aux = this.inicio;
+            int i = 1;
+
+            while ( ( i < pos - 1 ) && ( aux.proximo != null ) ){
+                aux = aux.proximo;
+                i++;
+            }
+
+            if ( i > 1 ){
+                deletado_nodo = aux.proximo;
+                aux.proximo = aux.proximo.proximo;
+                
+            }
+            else {
+                deletado_nodo = this.inicio;
+                this.inicio = deletado_nodo.proximo;
+            }
+            
+            deletado_nodo.proximo = null;
+            deletado_nodo = null;
+
+            this.size -= 1;
+        }
+    }
+
+
+    public String elementAt(int pos){
+        if ( pos >= this.size || pos < 0 )
+            return "nao ha esta posicao no vetor";
+
+        Nodo aux = this.inicio;
+        int i = 0;
+
+        while ( ( i < pos ) && ( aux.proximo != null ) ){
+            aux = aux.proximo;
+            i++;
+        }
+
+        return aux.elemento;
+    }
+
+
+    public void printList(){
+        if ( this.size > 0 ){
+
+            Nodo aux = this.inicio;
+            
+            while ( aux != null ){
+                System.out.print(aux.elemento + " ");
+                
+                aux = aux.proximo;
+            }
+            
+            System.out.println();
+        }
+        else {
+            System.out.println("Lista vazia");
+        }
+
+        System.out.println();
+    }
+
+
+    public boolean empty(){
+        return ( this.size == 0 )? true : false;
+    }
+
+
+    public int size(){
+        return this.size;
+    }
+}
