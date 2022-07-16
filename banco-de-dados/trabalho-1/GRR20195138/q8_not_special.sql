084314ba49005a4fc6103cd2a726d375d8540cd0
Tiago Serique <seriquevaladares29@gmail.com>
2022-07-15 21:31:18 -0300

    Conserta questao 8 do trab 1 de banco de dados

--- banco-de-dados/trabalho-1/GRR20195138/q8_not_special.sql
+++ banco-de-dados/trabalho-1/GRR20195138/q8_not_special.sql
@@ -1,9 +1,9 @@
 select count(*)
 from (
-    select distinct(c_custkey)
-    from customer
-    except
-    select distinct(c_custkey)
-    from customer inner join orders on c_custkey=o_custkey
-    where o_comment like '%special request%'
+	select distinct(c_custkey)
+	from customer inner join orders on c_custkey=o_custkey
+	except
+	select distinct(c_custkey)
+	from customer inner join orders on c_custkey=o_custkey
+	where o_comment like '%special request%'
 );
