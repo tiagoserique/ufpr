9e0e39054835fbbcbf0f3d8009049505cc06f9c7
tiagoserique <seriquevaladares29@gmail.com>
2022-07-21 11:23:00 -0300

    Adiciona exemplo de grafo completo e teste

--- algoritmos-e-teoria-dos-grafos/trabalho-1/grafo.c
+++ algoritmos-e-teoria-dos-grafos/trabalho-1/grafo.c
@@ -4,65 +4,101 @@
 //------------------------------------------------------------------------------
 grafo le_grafo(void) {
 
-  return agread(stdin, NULL); 
+    return agread(stdin, NULL); 
 }
 //------------------------------------------------------------------------------
 void destroi_grafo(grafo g) {
   
-  return 1;
+    agclose(g);
+    return;
 }
 //------------------------------------------------------------------------------
 grafo escreve_grafo(grafo g) {
   
-  return agwrite(g, stdout);
+    return agwrite(g, stdout);
 }
 
 // -----------------------------------------------------------------------------
 int n_vertices(grafo g) {
   
-  return 0;
+    return agnnodes(g);
 }
 
 // -----------------------------------------------------------------------------
 int n_arestas(grafo g) {
   
-  return 0;
+    return agnedges(g);
 }
 
 // -----------------------------------------------------------------------------
 int grau(vertice v, grafo g) {
   
-  return 0;
+    return agdegree(g, v, TRUE, TRUE);
 }
 
 // -----------------------------------------------------------------------------
 int grau_maximo(grafo g)  {
-  
-  return 0;
+    vertice nodo = agfstnode(g);
+
+    int max_grau = grau(nodo, g);
+
+    for (nodo = agnxtnode(g, nodo); nodo != NULL; nodo = agnxtnode(g, nodo)){
+        int grau_nodo_atual = grau(nodo, g);
+
+        if ( grau_nodo_atual > max_grau ) max_grau = grau_nodo_atual;
+    }
+
+    return max_grau;
 }
 
 // -----------------------------------------------------------------------------
 int grau_minimo(grafo g)  {
-  
-  return 0;
+    vertice nodo = agfstnode(g);
+
+    int min_grau = grau(nodo, g);
+
+    for (nodo = agnxtnode(g, nodo); nodo != NULL; nodo = agnxtnode(g, nodo)){
+        int grau_nodo_atual = grau(nodo, g);
+
+        if ( grau_nodo_atual < min_grau ) min_grau = grau_nodo_atual;
+    }
+
+    return min_grau;
 }
 
 // -----------------------------------------------------------------------------
 int grau_medio(grafo g) {
-  
-  return 0;
+    int grau_medio = 2 * n_arestas(g) / n_vertices(g);
+
+    return grau_medio;
 }
 
 // -----------------------------------------------------------------------------
 int regular(grafo g) {
-  
-  return 0;
+    vertice nodo = agfstnode(g);
+
+    int grau_regular = grau(nodo, g);
+
+    for (nodo = agnxtnode(g, nodo); nodo != NULL; nodo = agnxtnode(g, nodo)){
+        int grau_nodo_atual = grau(nodo, g);
+
+        if ( grau_nodo_atual != grau_regular ) return 0;
+    }
+
+    return 1;
 }
 
 // -----------------------------------------------------------------------------
 int completo(grafo g) {
-  
-  return 0;
+    int n_vertices_completo = n_vertices(g) - 1;
+
+    for (vertice nodo = agfstnode(g); nodo != NULL; nodo = agnxtnode(g, nodo)){
+        int grau_nodo_atual = grau(nodo, g);
+
+        if ( grau_nodo_atual != n_vertices_completo ) return 0;
+    }
+
+    return 1;
 }
 
 // -----------------------------------------------------------------------------
--- algoritmos-e-teoria-dos-grafos/trabalho-1/teste.c
+++ algoritmos-e-teoria-dos-grafos/trabalho-1/teste.c
@@ -5,11 +5,21 @@
 
 int main(void) {
 
-  grafo g = le_grafo();
+    grafo g = le_grafo();
 
-  escreve_grafo(g);
+    escreve_grafo(g);
 
-  destroi_grafo(g);
+    printf("\nn vertices = %d | n arestas = %d\n", n_vertices(g), n_arestas(g));
 
-  return 0;
+    printf("\ngrau maximo = %d | grau minimo = %d\n", grau_maximo(g), grau_minimo(g));
+
+    printf("\ngrau medio = %d\n", grau_medio(g));
+
+    printf("\nEh regular? %s\n", regular(g) ? "sim" : "nao");
+
+    printf("\nEh completo? %s\n", completo(g) ? "sim" : "nao");
+
+    destroi_grafo(g);
+
+    return 0;
 }
